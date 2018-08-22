ecc45c4b78d4191fb6faf274dd9fcdb83ed2d24a
cancha4202 <42418308+cancha4202@users.noreply.github.com>
2018-08-21 21:58:16 -0500

    Add files via upload

---
+++ academico.py
@@ -0,0 +1,18 @@
+sumaedades = 0
+promedio = 0
+edad1 = 15
+persona1 = ("Juan Carlos Arteaga")
+edad2 = 18
+persona2 = ("Angela Maria Lopez")
+edad3 = 22
+persona3 = ("Roberto Andres Jurado")
+edad4 = 19
+persona4 = ("Monica Maribel Hurtado")
+print ("Sistema de  Informacion Academico - Promedio de Edades")
+sumaedades = edad1+edad2+edad3+edad4
+print ("El estudiande" ,persona1," tiene " ,edad1," años")
+print ("El estudiande" ,persona2," tiene " ,edad2," años")
+print ("El estudiande" ,persona3," tiene " ,edad3," años")
+print ("El estudiande" ,persona4," tiene " ,edad4," años")
+promedio = sumaedades/4
+print ("El promedio de edades es: ",promedio)
---
+++ calc1.py
@@ -0,0 +1,18 @@
+n1 = 10
+n2 = 5
+print ("n1 = 10")
+print ("n2 = 5")
+print ("calculadora basica v1.0")
+primer_numero = n1
+segundo_numero = n2
+suma = n1+n2
+resta = n1-n2
+multiplicacion = n1*n2
+division = n1/n2
+print ("***********************")
+print ("resultados")
+print ("***********************")
+print ("la suma es: ", suma)
+print ("la suma es: ", resta)
+print ("la suma es: ", multiplicacion)
+print ("la suma es: ", division)
