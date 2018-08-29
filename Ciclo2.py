9530f95bb97a1c0f4d0146653023342ba22b16a6
cancha4202 <cb282001@gmail.com>
2018-08-29 14:28:30 -0500

    ciclos

---
+++ Ciclo2.py
@@ -0,0 +1,8 @@
+tabla = 1
+multip = 1
+while tabla <= 10 :
+    print (tabla ,"*", multip, "=", tabla * multip)
+    tabla = tabla + 1
+while multip <= 10 :
+    print (tabla ,"*", multip, "=", tabla * multip)
+    multip = multip + 1
---
+++ Ciclo3.py
@@ -0,0 +1,7 @@
+tabla = int(input("ingresar tabla a resolver: "))
+rango = int(input("ingresar el rango a multiplicar: ")
+num = 1
+while tabla <= 10 :
+    print (tabla, "*", multip, "=", tabla * multip)
+    tabla = tabla + 1
+
