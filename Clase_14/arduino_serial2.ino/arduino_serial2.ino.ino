0801c10a223636249e4e25bb7a3657362249be12
cancha4202 <cb282001@gmail.com>
2018-11-07 14:16:14 -0500

    Clase 14

--- Clase_14/arduino_serial2.ino/arduino_serial2.ino.ino
+++ Clase_14/arduino_serial2.ino/arduino_serial2.ino.ino
@@ -4,7 +4,7 @@ const int pinLED = 13;
 void setup() 
 {
   Serial.begin(9600);
-  pinMode(pinLED,OUTPUT)
+  pinMode(pinLED,OUTPUT);
 }
 
 void loop() 
@@ -12,15 +12,15 @@ void loop()
   if (Serial.available()>0)
   {
     char option = Serial.read();
-    if (option >= '1' && option <= 9
+    if (option >= '1' && option <= 9);
     {
-      option _= '0';
+      option -= '0';
       for(int i = 0;i<option;i++)
       {
         digitalWrite(pinLED, HIGH);
         delay (100);
-        digitalWrite(pinLED, LOW)
-        delay (200)
+        digitalWrite(pinLED, LOW);
+        delay (200);
       }
     }
   }
--- Clase_14/python_serial2.py
+++ Clase_14/python_serial2.py
@@ -1,5 +1,5 @@
 import serial, time
 arduino = serial.Serial("COM3",9600)
 time.sleep(2)
-arduino.write(b'9')
+arduino.write(b'3')
 arduino.close()
