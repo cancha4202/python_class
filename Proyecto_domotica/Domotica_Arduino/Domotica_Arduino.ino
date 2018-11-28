2c429fa23b540115cb79d9e64e0f0acc364c5a34
os2var <os2varpla@gmail.com>
2018-11-28 00:12:59 -0500

    28 nov 2018

--- Proyecto_domotica/Domotica_Arduino/Domotica_Arduino.ino
+++ Proyecto_domotica/Domotica_Arduino/Domotica_Arduino.ino
@@ -1,34 +1,167 @@
-char opc = '0';
-const int Leds = 1 ;
-const int Motor = 2;
-const int Servos = 3;
-const int Fan = 4;
-float Temper°C;
+char Valor;
+#include <Servo.h>
 
-int pinSensorT = 0;
 
-void setup() 
+Servo ServoPP;
+Servo ServoPG;
+
+
+int angleC = 0;
+int angleA = 90 ;
+int i;
+const int LuzCocina = 0;
+const int LuzSala = 1;
+const int LuzGaraje = 2;
+const int LuzBano = 3;
+const int LuzAlcoba = 4;
+const int LuzLavadero = 9;
+const int Fogon1 = 5;
+const int Fogon2 = 6;
+const int Fogon3 = 7;
+const int Fogon4 = 8;
+const int TempV = 10;
+const int TempC = 11;
+const int Extractor = 12;
+const int Ventilador = 13;
+
+
+
+void setup()
 {
   Serial.begin(9600);
-  pinMode (Leds,OUTPUT):
-  pinMode (Motor,OUTPUT):
-  pinMode (Servos,OUTPUT):
-  pinMode (Fans,OUTPUT):
-}
+  
+  pinMode(LuzCocina, OUTPUT);
+  pinMode(LuzBano, OUTPUT);
+  pinMode(LuzLavadero, OUTPUT);
+  pinMode(LuzAlcoba, OUTPUT);
+  pinMode(LuzSala, OUTPUT);
+  pinMode(LuzGaraje, OUTPUT);
+  pinMode(TempV, OUTPUT);
+  pinMode(TempC, OUTPUT);
+  pinMode(Extractor, OUTPUT);
+  pinMode(Ventilador, OUTPUT);
+  pinMode(Fogon1, OUTPUT);
+  pinMode(Fogon2, OUTPUT);
+  pinMode(Fogon3, OUTPUT);
+  pinMode(Fogon4, OUTPUT);
 
+
+  
+
+}
+ 
 void loop()
 {
-  if (Serial.available())
-  
-   {switch(Serial.read()){
-    
-    case'0':digitalWrite(13,LOW);
-      break;
-    case'1':digitalWrite(13,HIGH);
-      break;
-
-    default: Break;
+   if(Serial.available()>0)
+   {
+    Valor=Serial.read();
+    Serial.print(Valor);
+   }
+    else if (Valor == '0')
+    {
+      digitalWrite(LuzSala, HIGH);
     }
-  }
-}
-  
+    else if (Valor == '0')
+    {
+      digitalWrite(LuzSala, LOW);
+    }      
+    else if (Valor == '1')
+    {
+      digitalWrite(LuzBano, HIGH);
+    }
+    else if (Valor == '1')
+    {
+      digitalWrite(LuzBano, LOW);
+    }
+        else if (Valor == '2')
+    {
+      digitalWrite(LuzCocina, HIGH);
+    }
+    else if (Valor == '2')
+    {
+      digitalWrite(LuzCocina, LOW);
+    }
+    else if (Valor == '3')
+    {
+      digitalWrite(LuzGaraje, HIGH);
+    }
+    else if (Valor == '3')
+    {
+      digitalWrite(LuzGaraje, LOW);
+    }      
+    else if (Valor == '4')
+    {
+      ServoPG.write (angleA);
+      delay(6000);
+    }
+    else if (Valor == '4')
+    {
+      ServoPG.write (angleC);
+      delay(6000);
+    }      
+    else if (Valor == '5')
+    {
+      ServoPP.write (angleA);
+      delay(6000);
+    }
+    else if (Valor == '5')
+    {
+      ServoPP.write (angleC);
+      delay(6000);
+    }
+
+    else if (Valor == '6')
+    {
+      for  (i=1;i<255; i++){
+      analogWrite(Fogon1,i);
+      delay(5);
+      }
+    else if (Valor == '6')
+    {
+      for  (i=255;i>0; i--){
+      analogWrite(Fogon1,i);
+      delay(5);
+      }      
+    else if (Valor == '7')
+    {
+      for  (i=1;i<255; i++){
+      analogWrite(Fogon2,i);
+      delay(5);
+      }
+    else if (Valor == '7')
+    {
+      for  (i=255;i>0; i--){
+      analogWrite(Fogon2,i);
+      delay(5);
+      }
+      
+    else if (Valor == '8')
+    {
+      for  (i=1;i<255; i++){
+      analogWrite(Fogon3,i);
+      delay(5);
+      }
+    else if (Valor == '8')
+    {
+      for  (i=255;i>0; i--){
+      analogWrite(Fogon3,i);
+      delay(5);
+      }    
+    else if (Valor == '9')
+    {
+      for  (i=1;i<255; i++){
+      analogWrite(Fogon4,i);
+      delay(5);
+      }
+    else if (Valor == '9')
+    {
+      for  (i=255;i>0; i--){
+      analogWrite(Fogon4,i);
+      delay(5);
+      }
+      
+//    else if (Valor == "10")
+//    {
+      
+//    }
+    
--- Proyecto_domotica/Domotica_base_python.py
+++ Proyecto_domotica/Domotica_base_python.py
@@ -11,20 +11,20 @@ import tkinter
 
 #Crear ventana
 ventana_login = tkinter.Tk()
-ventana_login.title("Login")#Nombre ventana
-ventana_login.geometry("500x500") #WExHE or ALxAN
-ventana_login.resizable(FALSE,FALSE) #define si se puede o no cambiar el tamaño de la paguinaWExHE or ALxAN
-ventana_login.configure(background="#FFFFFF") #color fondo
+ventana_login.title("Login")
+ventana_login.geometry("500x500")
+ventana_login.resizable(FALSE,FALSE)
+ventana_login.configure(background="#FFFFFF")
 color_boton=("gray77")
 
 #FUNCIONES
 
 def encendido(valor):
     arduinoData.write(b'valor')
-    print ("Encendido")
+    #print ("Encendido")
 def apagado(valor):
     arduinoData.write(b'valor')
-    print("Apagado")
+    #print("Apagado")
 
 def Leds():
     print ("funciona")
@@ -35,6 +35,7 @@ def MainM():
     ventana_2.title("Menu de Control")
     ventana_2.geometry("400x400")
     ventana_2.resizable(FALSE,FALSE)
+    ventana_2.configure(background="#000000")
     Btn_Menu_Sala = Button(ventana_2, text = "Sala", width = 15 , height = 2, command = Sala).place(x=30,y=30)
     Btn_Menu_Garaje = Button(ventana_2, text = "Garaje", width = 15, height = 2, command = Garaje).place(x=30,y=100)
     Btn_Alcoba = Button(ventana_2, text = "Alcoba", width = 15 , height = 2, command = Alcoba).place(x=150,y=30)
@@ -48,8 +49,8 @@ def Sala():
     ventana_3.geometry("400x400")
     ventana_3.resizable(FALSE,FALSE)
     ventana_3.configure(background="#000000")
-    Btn_Luz = Button(ventana_3, text = "Luz", width = 5 , height = 2, command = lambda:encendido(2)).place(x=30,y=30)
-    Btn_Puerta = Button(ventana_3, text = "Puerta Principal", width = 5 , height = 2, command = lambda:encendido(3)).place(x=100,y=30)
+    Btn_LuzSala = Button(ventana_3, text = "Luz Sala", width = 5 , height = 2, command = lambda:encendido(2)).place(x=30,y=30)
+    Btn_PuertaP = Button(ventana_3, text = "Puerta Principal", width = 5 , height = 2, command = lambda:encendido(3)).place(x=100,y=30)
     Btn_Luz_Cocina = Button(ventana_3, text = "Luz Cocina", width = 5 , height = 2, command = lambda:encendido(4)).place(x=170,y=30)
     Btn_Estufa = Button(ventana_3, text = "Estufa", width = 5 , height = 2, command = Estufa).place(x=240,y=30)
 
@@ -60,22 +61,23 @@ def Servos():
     ventana_4.geometry("175x130")
     ventana_4.resizable(FALSE,FALSE)
     ventana_4.configure(background="#000000")
-    Btn_Puerta_Principal = Button(ventana_4, text = "Ventilador 1", width = 15 , height = 2, command = lambda:encendido(0)).place(x=31.5,y=15)
-    Btn_Garaje = Button(ventana_4, text = "Ventilador 2", width = 15 , height = 2, command = lambda:encendido(1)).place(x=31.5,y=75)
+    Btn_Puerta_Principal = Button(ventana_4, text = "Ventilador 1", width = 15 , height = 2, command = lambda:encendido(5)).place(x=31.5,y=15)
+    Btn_Garaje = Button(ventana_4, text = "Ventilador 2", width = 15 , height = 2, command = lambda:encendido(6)).place(x=31.5,y=75)
 
 def Estufa():
     global ventana_5
     ventana_5 = tkinter.Tk()
-    ventana_5.title("Menu de Control")
+    ventana_5.title("Menu de Cocina")
     ventana_5.geometry("175x130")
     ventana_5.resizable(FALSE,FALSE)
+    ventana_5.configure(background="#000000")
     Btn_extractor = Button(ventana_5, text = "Ventilador 1", width = 15 , height = 2, command = lambda:encendido(6)).place(x=31.5,y=15)
     Btn_Fogon = Button(ventana_5, text = "Ventilador 2", width = 15 , height = 2, command = Fogon).place(x=31.5,y=75)
 
 def Garaje():
     global ventana_6
     ventana_6 = tkinter.Tk()
-    ventana_6.title("Menu de Control")
+    ventana_6.title("Garaje")
     ventana_6.geometry("175x130")
     ventana_6.resizable(FALSE,FALSE)
     Btn_Luz = Button(ventana_6, text = "Luz", width = 15 , height = 2, command = lambda:encendido(14)).place(x=31.5,y=15)
